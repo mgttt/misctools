8667f5ddbe9946d6751f70292f24e05925589185
WANJO CHAN <wanjochan@users.noreply.github.com>
2016-07-02 19:19:02 +0800

    Update php-fpm-swoole-one-click.sh

--- php-fpm-swoole-one-click.sh
+++ php-fpm-swoole-one-click.sh
@@ -1,25 +1,30 @@
-# php swoole dl compile link php-fpm install quick sh
-# php-fpm-swoole-one-click.sh
+# Auth: Wanjo Chan
+# Purpose: php swoole dl compile link php-fpm install quick sh
+# Usage: wget -q https://github.com/wanjochan/misctools/raw/master/php-fpm-swoole-one-click.sh -O - | sh
 
+# TODO if args provided then use args --PHPVER, and also the PHPDL and target folder
 PHPVER="7.0.8"
 PHPDL="http://hk.php.net/distributions/"
 mkdir $HOME/php7/
 cd $HOME/php7/
+
+# TODO skip if file exists and checksum currect? (download checksume also...)
 wget http://hk1.php.net/distributions/php-$PHPVER.tar.gz -o php-${PHPVER}.tgz
 ls -al php-${PHPVER}.tgz
 tar xzvf php-$PHPVER.tar.gz
 cd php-$PHPVER/
 
-
+# TODO more PHPCONF 
 ./configure \
 --prefix=$HOME/opt/php7 \
+--enable-fpm \
 --enable-opcache \
 --with-openssl \
 --with-system-ciphers \
 --with-zlib \
 --with-curl \
 --with-pcre-dir \
---enable-ftp \
+--with-pcre-regex \
 --with-zlib-dir \
 --with-mysqli \
 --with-mysql-sock \
