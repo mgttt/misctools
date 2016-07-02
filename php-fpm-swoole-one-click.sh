6bb38edcf645358fa687380cf68dcc868623fc25
WANJO CHAN <wanjochan@users.noreply.github.com>
2016-07-02 19:08:09 +0800

    Create php-fpm-swoole-one-click.sh

---
+++ php-fpm-swoole-one-click.sh
@@ -0,0 +1,41 @@
+# php swoole dl compile link php-fpm install quick sh
+
+echo PHPVER="7.0.8"
+echo PHPDL="http://hk.php.net/distributions/"
+echo mkdir $HOME/php7/
+echo cd $HOME/php7/
+echo wget http://hk1.php.net/distributions/php-$PHPVER.tar.gz -o php-${PHPVER}.tgz
+echo ls -al php-${PHPVER}.tgz
+echo tar xzvf php-$PHPVER.tar.gz
+echo cd php-$PHPVER/
+
+
+echo ./configure \
+--prefix=$HOME/opt/php7 \
+--enable-opcache \
+--with-openssl \
+--with-system-ciphers \
+--with-zlib \
+--with-curl \
+--with-pcre-dir \
+--enable-ftp \
+--with-zlib-dir \
+--with-mysqli \
+--with-mysql-sock \
+--with-zlib-dir \
+--enable-embedded-mysqli \
+--with-pdo-mysql \
+--enable-soap \
+--enable-sockets \
+--enable-zip \
+--enable-mysqlnd \
+--with-pear
+
+make && make install
+
+# swoole
+
+$HOME/opt/php7/bin/pear config-set download_dir $HOME/php7/
+$HOME/opt/php7/bin/pear config-set temp_dir $HOME/php7/
+
+$HOME/opt/php7/bin/pecl install swoole
