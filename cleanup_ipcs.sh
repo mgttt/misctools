fd21d90a74fdf8fa8eab46ee486fa23f7eb9a5fe
WANJO CHAN <wanjochan@users.noreply.github.com>
2016-08-25 00:46:12 +0800

    Create cleanup_ipcs.sh

---
+++ cleanup_ipcs.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+ipcs -m | grep `whoami` | awk '{ print $2 }' | xargs -n1 ipcrm -m
+ipcs -s | grep `whoami` | awk '{ print $2 }' | xargs -n1 ipcrm -s
+ipcs -q | grep `whoami` | awk '{ print $2 }' | xargs -n1 ipcrm -q
+ipcs
