1316a863fe3695f0c220596767e13369222286de
WANJO CHAN <wanjochan@users.noreply.github.com>
2017-10-08 07:49:05 +0800

    progressing download hint

--- nodejs_related/node_quick_download.js
+++ nodejs_related/node_quick_download.js
@@ -1,20 +1,16 @@
-function argv2o(argv,m,mm){var rt={};for(k in argv)(m=(rt[""+k]=argv[k]).match(/^(\/|--?)([a-zA-Z0-9-_]*)="?(.*)"?$/))&&(rt[m[2]]=m[3]);return rt}
+function argv2o(argv,m,k,rt){rt={};for(k in argv)(m=(rt[""+k]=argv[k]).match(/^(\/|--?)([a-zA-Z0-9-_]*)="?(.*)"?$/))&&(rt[m[2]]=m[3]);return rt}
 var argo=argv2o(process.argv);
 var url=argo.url;
 var filename=argo.filename;
 if(filename && url){
-	var m=url.match(/^(https?):\/\/([^\/]*)/);
-	var os=require('os');
-	var fs=require('fs');
-	var tmpdir=os.tmpdir();
-	var web=require(m[1]);
-	var file = fs.createWriteStream(filename);
-	web.get(url, function( res ){
+	var file = require('fs').createWriteStream(filename);
+	require(url.match(/^(https?):\/\/([^\/]*)/)[1]).get(url, res=>{
+		res.on('data', d=>{
+			process.stdout.write('.');
+		});
 		res.pipe(file);
-		file.on('finish', function() {
-			file.close(function(){
-				console.log('finish');
-			});
+		file.on('finish', ()=>{
+			file.close(()=>{ console.log('d/l done'); })
 		});
 	});
 }else{
